5ad9f1985ce238c7e1be482763e052566cce6271
chris.uribe.is@gmail.com <elizabeth@Elizabeths-MBP.localdomain>
2024-02-24 15:48:27 -0800

    update vite for Github

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Update base for Github repository
+const base = process.env.NODE_ENV === 'production' ? '/agilecoderquest/' : '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
