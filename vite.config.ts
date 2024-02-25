03dee308ba7a5fc5271ba087da94b7523588890b
chris.uribe.is@gmail.com <elizabeth@Elizabeths-MBP.localdomain>
2024-02-25 08:25:28 -0800

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Update base for Github repository
-const base = process.env.NODE_ENV === 'production' ? '/agilecoderquest/' : '/';
+//const base = process.env.NODE_ENV === 'production' ? '/agilecoderquest/' : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base,
+  //base,
   plugins: [react()],
   build: {
     outDir: 'build', // Set the output directory to 'build'
