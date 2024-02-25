016a25331e354c8d87564ca1c61a12098d21d2e3
chris.uribe.is@gmail.com <elizabeth@Elizabeths-MBP.localdomain>
2024-02-24 16:19:08 -0800

    update vite base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const base = process.env.NODE_ENV === 'production' ? '/agilecoderquest/' : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
   build: {
     outDir: 'build', // Set the output directory to 'build'
