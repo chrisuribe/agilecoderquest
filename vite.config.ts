b7e19935934f12d696dfd23c34f49759ebc80c40
chris.uribe.is@gmail.com <elizabeth@Elizabeths-MBP.localdomain>
2024-02-25 10:04:31 -0800

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base,
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'build', // Set the output directory to 'build'
