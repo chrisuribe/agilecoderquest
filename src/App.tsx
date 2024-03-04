1040042816c143d5f9f0ba6b00660a80ea7ec0c2
Chris Uribe <chris.uribe.is@gmail.com>
2024-03-03 21:45:38 -0800

    moved welcome from main to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Welcome from './Welcome'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <h1>H1 on page: Loaded at 4:44</h1>
-      <p>This page is just testing that the React page is being correctly created and that JS is working.</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Welcome />
     </>
   )
 }
