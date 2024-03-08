ad0377324378773da1149f2c0fda1ac06833bbbf
Chris Uribe <chris.uribe.is@gmail.com>
2024-03-08 14:38:44 -0800

    added basic structure using text

--- src/components/game/GameArea.tsx
+++ src/components/game/GameArea.tsx
@@ -26,13 +26,65 @@ const GameArea = () => {
           'radial-gradient(60% 400px at 50% 60%,#841bad 5%,#371761 100%)',
         perspective: '500px',
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
       }}
     >
       {/* All words:
         <DisplayWords words={words} /> */}
 
+      {/* HEADER AREA - This is the area where the menu, round, score, and coundown timer component will go */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          width: '100%',
+          margin: '20px',
+        }}
+      >
+        <div>Menu ≡</div>
+        <div>Round: 1</div>
+        <div>Score: 0</div>
+        <div>Time: 03:30</div>
+        <div>Pause Button</div>
+      </div>
+
+      {/* BOARD AREA - this is where completed words, bonus letters, and empty letter slots will go */}
+      <div
+        style={{
+          display: 'flex',
+          gap: '20px',
+          fontSize: 'xx-large',
+        }}
+      >
+        <div>
+          <div>☐☐☐</div>
+          <div>☐☐☐</div>
+          <div>☐☐☐☐</div>
+        </div>
+        <div>
+          <div>☐☐☐☐</div>
+          <div>T☐☐☐</div>
+          <div>A☐☐☐☐</div>
+        </div>
+      </div>
+
+      {/* Round Progress Meter - This is where we will show how much progress is needed to get to the next round.  */}
+      <div> 25% - 50% - 75% - 100% - Complete </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          width: '100%',
+        }}
+      >
+        <div>clear screen button</div>
+        <div> screen /_______________________\</div>
+        <div>return letter button</div>
+      </div>
+
+      {/* TILE HOLDER - This component will hold the tiles. When a letter is sellected a callback function will be called. if letters are added or removed, then we just updated  */}
       <div className="tile-holder">
         <TileHolder
           letters={currentLetters}
@@ -40,6 +92,23 @@ const GameArea = () => {
             console.log('selected: ' + letter);
           }}
         />
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          width: '100%',
+          margin: '20px',
+        }}
+      >
+        <div style={{ flex: 1, marginLeft: '20px' }}>shuffle tiles button </div>
+        <div
+          style={{
+            flex: 2,
+          }}
+        >
+          enter word button
+        </div>
       </div>
     </div>
   );
