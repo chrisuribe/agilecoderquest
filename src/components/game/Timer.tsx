5df7a70ae12cf47f21ed7b64948989466fa39292
Chris Uribe <chris.uribe.is@gmail.com>
2024-03-08 16:49:01 -0800

    working on pause button and time logic

--- src/components/game/Timer.tsx
+++ src/components/game/Timer.tsx
@@ -4,6 +4,7 @@ import { Button } from '@mui/material';
 import PauseIcon from '@mui/icons-material/Pause';
 
 interface Props {
+  // these are callback functions that will be called when time is up or when the pause button is pressed.
   timeIsUp: () => void;
   pausePressed: (isRunning: boolean) => void;
 }
@@ -37,7 +38,9 @@ const Timer = forwardRef<TimerHandle, Props>(
     }));
 
     const handlePausePressed = () => {
-      pausePressed(time > 0);
+      const isRunning = false; // TODO: i just put false for now, but we need logic to tell if were running or not.... maybe a state variable that can tell if we're in the paused state or not? also is time > 0 ??? something like that.
+      pausePressed(isRunning);
+      //TODO: add logic to pause time and also
     };
 
     return (
